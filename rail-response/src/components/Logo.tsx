1bfac4b8f07135af788da8ab2ebb59f8719a06b0
alanerochaa <alanerochaa@gmail.com>
2025-05-22 11:09:41 -0300

    feat: componentes dashboard card, logo e lógica (HOC)

---
+++ rail-response/src/components/Logo.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+interface LogoProps {
+    className?: string;
+}
+
+export function Logo({ className = "" }: LogoProps) {
+    return (
+        <Image
+            src="/imagens/logo.png" // caminho relativo à pasta 'public'
+            alt="Logo RailResponse"
+            width={200}
+            height={200}
+            className={`rounded-full ${className}`}
+        />
+    );
+}
